b59b96a83148cce3e5d96c005ead7d044334e15e
Amit <achaudhary8118@gmail.com>
2021-04-15 17:06:22 +0530

    First commit

---
+++ src/main/java/javaprojectpractice/Mavenjava/App.java
@@ -0,0 +1,13 @@
+package javaprojectpractice.Mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/javaprojectpractice/Mavenjava/Selenium.java
@@ -0,0 +1,11 @@
+package javaprojectpractice.Mavenjava;
+
+public class Selenium {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Selenium Maven Method code");
+	} 
+	
+}
+	
